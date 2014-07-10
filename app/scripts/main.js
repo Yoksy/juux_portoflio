f82f56adea84c954ade5a21f556f611a01f32870
Juux <mac@Juux-3.local>
2014-07-11 01:12:19 +0200

    coucou

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
-console.log('\'Allo \'Allo ici la terre!');
+$(document).ready(function(){
+	scrollAnchor();
+});
+
+function scrollAnchor() {
+	$('a[href^=#]').on("click",function(e){
+	    var t= $(this.hash);
+	    var t=t.length&&t||$('[name='+this.hash.slice(1)+']');
+	    if(t.length){
+	        var tOffset=t.offset().top;
+	        $('html,body').animate({scrollTop:tOffset-20},'slow');
+	        e.preventDefault();
+	    }
+	})
+}
